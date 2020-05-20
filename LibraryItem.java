adf3f133f924ee4354247c59cff10ce2732b61b7
tempesttswinson <tempestt.swinson@gmail.com>
2020-05-20 16:38:02 -0400

    delte duplicated constuctor in LibraryItem class

--- LibraryItem.java
+++ LibraryItem.java
@@ -1,4 +1,81 @@
 
 public class LibraryItem {
 
+    // properties
+    private boolean isAvailable, isOverdue;
+    private String borrower;// find a way to intergrate Card Holder class
+    private String checkOutDate, returnDate;
+    private int numOfCopies;
+
+    // default constuctor
+    public LibraryItem(boolean isAvailable, boolean isOverdue, String borrower, String checkOutDate, String returnDate,
+            int numOfCopies) {
+        this.isAvailable = isAvailable;
+        this.isOverdue = isOverdue;
+        this.borrower = borrower;
+        this.checkOutDate = checkOutDate;
+        this.returnDate = returnDate;
+        this.numOfCopies = numOfCopies;
+    }
+
+    // constuctor for when the item has not be borrowed by anyone
+    public LibraryItem(boolean isAvailable, int numOfCopies) {
+        this.isAvailable = isAvailable;
+        this.numOfCopies = numOfCopies;
+    }
+
+    // getters & setters
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public boolean isOverdue() {
+        return isOverdue;
+    }
+
+    public void setOverdue(boolean isOverdue) {
+        this.isOverdue = isOverdue;
+    }
+
+    public String getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(String borrower) {
+        this.borrower = borrower;
+    }
+
+    public String getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(String checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public int getNumOfCopies() {
+        return numOfCopies;
+    }
+
+    public void setNumOfCopies(int numOfCopies) {
+        this.numOfCopies = numOfCopies;
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryItem [isAvailable=" + isAvailable + ", numOfCopies=" + numOfCopies + "]";
+    }
+
 }
