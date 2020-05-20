b985204d73b7b9c86dfe8d3d230775f89f20e764
tempesttswinson <tempestt.swinson@gmail.com>
2020-05-20 18:56:48 -0400

    edit the constuctor of LibraryItem and completed consuctor, getters and setters for periodical

--- Book.java
+++ Book.java
@@ -1,23 +1,13 @@
 public class Book extends LibraryItem {
 
-    private String title, genre, location;
+    private String genre;
     private int numOfPages;
 
     public Book(boolean isAvailable, boolean isOverdue, String borrower, String checkOutDate, String returnDate,
             int numOfCopies, String title, String genre, String location, int numOfPages) {
-        super(isAvailable, isOverdue, borrower, checkOutDate, returnDate, numOfCopies);
-        this.title = title;
+        super(isAvailable, isOverdue, borrower, checkOutDate, returnDate, numOfCopies, title, location);
         this.genre = genre;
-        this.location = location;
         this.numOfPages = numOfPages;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getGenre() {
@@ -26,14 +16,6 @@ public class Book extends LibraryItem {
 
     public void setGenre(String genre) {
         this.genre = genre;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
     }
 
     public int getNumOfPages() {
--- Periodical.java
+++ Periodical.java
@@ -1,3 +1,29 @@
 public class Periodical extends LibraryItem {
 
+    private int issueNum;
+    private String issueFreq;// implement enums
+
+    public Periodical(boolean isAvailable, boolean isOverdue, String borrower, String checkOutDate, String returnDate,
+            int numOfCopies, String location, String title, int issueNum, String issueFreq) {
+        super(isAvailable, isOverdue, borrower, checkOutDate, returnDate, numOfCopies, location, title);
+        this.issueNum = issueNum;
+        this.issueFreq = issueFreq;
+    }
+
+    public int getIssueNum() {
+        return issueNum;
+    }
+
+    public void setIssueNum(int issueNum) {
+        this.issueNum = issueNum;
+    }
+
+    public String getIssueFreq() {
+        return issueFreq;
+    }
+
+    public void setIssueFreq(String issueFreq) {
+        this.issueFreq = issueFreq;
+    }
+
 }
